2c5193148a14f85373d645f9d0a8fbc91cce019e
caioit <56567458+caioit@users.noreply.github.com>
2021-06-03 00:36:08 -0300

    Views funcionando

---
+++ SWPets/SWPets/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SWPets.MvcApplication" Language="C#" %>
---
+++ SWPets/SWPets/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SWPets.Startup))]
+namespace SWPets
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
